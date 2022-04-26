a9521e0bcfcf6018716bba1c28a1495a9a7173fd
peng49 <1219955253@qq.com>
2022-04-26 15:02:50 +0800

    merge master

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # php开发镜像
 php + nginx
 
+|分支|说明|
+|:---:|:---:|
+|dev|开发环境|
+|pro|生成环境,移除了开发环境的 git 和 composer|
 
+## 使用示例
+启动一个laravel项目
+
+```shell
+docker run -it -d -p 8080:80 -v F://laravel:/var/www/html peng49/php:dev 
+```
--- setting.sh
+++ setting.sh
@@ -3,12 +3,14 @@
 cat <<EOF | tee /etc/nginx/nginx.conf
 user  nginx;
 worker_processes  auto;
+worker_rlimit_nofile 65535;
 
 error_log  /var/log/nginx/error.log notice;
 pid        /var/run/nginx.pid;
 
 events {
-    worker_connections  1024;
+    worker_connections  4096;
+    use epoll;
 }
 
 http {
@@ -21,12 +23,13 @@ http {
 
     access_log  /var/log/nginx/access.log  main;
 
+    server_tokens   off;
     sendfile        off;
     #tcp_nopush     on;
 
     keepalive_timeout  65;
 
-    #gzip  on;
+    gzip  on;
 
     include /etc/nginx/conf.d/*.conf;
 }
@@ -39,9 +42,21 @@ server {
 
     #access_log  /var/log/nginx/host.access.log  main;
 
+    if (\$http_x_forwarded_for = "" ) {
+        set \$client_ip "\$remote_addr";
+    }
+    if (\$http_x_forwarded_for != "" ) {
+        set \$client_ip "\$http_x_forwarded_for";
+    }
+    if (\$http_user_agent = "kube-probe/1.11") {
+        access_log off;
+    }
+
     root /var/www/html/public/;
 
     index index.php index.html;
+
+    client_max_body_size    0;
 
     location / {
         try_files \$uri \$uri/ /index.php?\$query_string;
@@ -57,6 +72,28 @@ server {
         fastcgi_index  index.php;
         fastcgi_param  SCRIPT_FILENAME  \$document_root\$fastcgi_script_name;
         include        fastcgi_params;
+
+        proxy_read_timeout 1800s;
+        proxy_connect_timeout 120;
+        proxy_send_timeout 180;
+        fastcgi_max_temp_file_size 0;
+        fastcgi_buffer_size 4K;
+        fastcgi_buffers 32 8k;
+        client_body_buffer_size 1024k;
+    }
+
+    location ~ .*\.(gif|jpg|jpeg|png|bmp|swf|ico)$ {
+        expires 7d;
+        access_log  off;
+    }
+
+    location ~ .*\.(js|css)$ {
+        expires 12h;
+        access_log  off;
+    }
+
+    location ~ /\.ht {
+        deny  all;
     }
 }
 EOF
